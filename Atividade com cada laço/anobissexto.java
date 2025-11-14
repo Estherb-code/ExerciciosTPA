53b0c51cd0e784b69903c7c53df302a36046eae2
Estherb-code <beatrizesther.brandao@gmail.com>
2025-11-14 08:04:01 -0300

    Add files via upload

---
+++ Atividade com cada laço/anobissexto.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class anobissexto {
+	 public static void main(String[] args) {
+	   Scanner ler = new Scanner(System.in);
+	    
+	   int anoInicial;
+	   int anoFinal;
+	   int anos = 0;
+	   int anosBissextos = 0;
+	    
+	   
+	    
+	   System.out.println("Digite o ano inicial: ");
+	   anoInicial = ler.nextInt();
+	    
+	   System.out.println("Digite o ano final: ");
+	   anoFinal = ler.nextInt();
+	    
+	   
+	   while (anoInicial<anoFinal) {
+	    
+	       if (anoInicial % 4 == 0) {
+	    
+	                anosBissextos = anoInicial;
+	       
+	       System.out.println("Os anos bissextos são: " + anosBissextos);
+	    
+	     }
+	    
+	         anos = anos + 1;
+	      anoInicial = anoInicial + 1;
+	    
+	   }
+	    
+	   System.out.println("O total de anos dentro do intervalo é de: " + anos);
+	    
+	   
+	    
+	   }
+	    
+
+	 }
+	 
+
---
+++ Atividade com cada laço/idadeUsuario.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class idadeUsuario {
+
+	 public static void main(String[] args) {
+	  Scanner in = new Scanner(System.in);
+	   
+	     int anoAtual;
+	   
+	  int anoNascimento;
+	   
+	  int idade;
+	   
+	        int i;
+	   
+	  
+	        do {
+	    
+	  System.out.println("Digite o ano de nascimento: ");
+	  anoNascimento = in.nextInt();
+	   
+	  System.out.println("Digite o ano atual: ");
+	  anoAtual = in.nextInt();
+	   
+	  
+	  idade = anoAtual - anoNascimento;
+	   
+	  
+	  System.out.println("A idade é: "+ idade);
+	   
+	        System.out.println("Deseja continuar? 1 Continuar / 0 - sair");
+	   
+	  i = in.nextInt();
+	   
+	  } while (i == 1);
+	   
+	   
+	 }
+	}
+
